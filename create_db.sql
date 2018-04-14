39bc57a2903ba9059b1f398839f82f79268f632e
hokolomopo <adrien.minne@outlook.com>
2018-04-14 18:31:25 +0200

    Add UTF-8 encodage

--- create_db.sql
+++ create_db.sql
@@ -30,14 +30,14 @@ CREATE TABLE IF NOT EXISTS Animal (
 	nom_scientique VARCHAR(40) NOT NULL,
     n_puce SMALLINT UNSIGNED NOT NULL,
     taille INT NOT NULL,
-    sexe CHAR(1) NOT NULL,
+    sexe CHAR(1) NOT NULL CHECK (sexe IN ('F', 'M')),
     date_naissance DATETIME NOT NULL,
     n_enclos SMALLINT NOT NULL,
     PRIMARY KEY (n_puce, nom_scientique),
     FOREIGN KEY (nom_scientique)             
         REFERENCES Espece(nom_scientique),
     FOREIGN KEY (n_enclos)             
-        REFERENCES Enclos(n_enclos)
+        REFERENCES Enclos(n_enclos),
 )ENGINE=INNODB;
 SHOW TABLES;
 
