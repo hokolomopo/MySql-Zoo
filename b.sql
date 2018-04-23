35a91b2c2483e7d0ac70b2d5c21cc112fc8cdf0d
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-04-23 21:35:45 +0200

    solve b.sql

--- b.sql
+++ b.sql
@@ -4,9 +4,9 @@ FROM
 	Animal 
 	NATURAL JOIN 
 	(
-		SELECT nom_scientique, n_puce, count(distinct n_registre) as nb_intervention 
+		SELECT nom_scientifique, n_puce, count(distinct n_registre) as nb_intervention 
 		FROM Intervention 
-		GROUP BY nom_scientique, n_puce
+		GROUP BY nom_scientifique, n_puce
 	) as t1
 ) 
 ORDER BY nb_intervention;
