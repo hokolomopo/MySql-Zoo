82b99013a5dcce6f5ce37fe3412fa8f51d93e200
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-04-23 13:22:57 +0200

    correction de l'ajout d'animal

--- e_ajoute_animal.sql
+++ e_ajoute_animal.sql
@@ -1,1 +1,1 @@
-Insert into Animal Values(:nom_scientifique, :n_puce, :taille, :sexe, :date_naissance, :n_enclos, NOW());
+Insert into Animal Values(:nom_scientifique, :n_puce, :taille, :sexe, :date_naissance, :n_enclos);
